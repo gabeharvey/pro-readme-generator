da013dbe8ce0cbd8d95ef33e9f569f11d333c020
Gabe Harvey <harvey7@gmail.com>
2024-01-29 13:54:16 -0600

    questions and console.log info updated in index.js

--- develop/index.js
+++ develop/index.js
@@ -14,43 +14,43 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'Please provide a description of your application.',
+        message: 'Please provide a description of your application:',
         name: 'description',
         default: 'Enter Description',
     },
     {
         type: 'input',
-        message: 'Please provide installation instructions. If no installation required please enter "N/A".',
+        message: 'Please provide installation instructions. If no installation required please enter "N/A":',
         name: 'installation',
         default: 'Enter Installation Instructions',
     },
     {
         type: 'input',
-        message: 'Please provide usage instructions for the user. If no usage instructions please enter "N/A"',
+        message: 'Please provide usage instructions for the user. If no usage instructions please enter "N/A":',
         name: 'usage',
         default: 'Enter Usage Instructions',
     },
     {
         type: 'input',
-        message: 'Please list any credits and resources utilized on the application. This also includes any collaborations. If no resources used please enter "N/A"',
+        message: 'Please list any credits and resources utilized on the application. This also includes any collaborations. If no resources used please enter "N/A":',
         name: 'credits',
         default: 'Enter Credits',
     },
     {
         type: 'input',
-        message: 'Please include a screenshot of your application',
+        message: 'Please include a screenshot of your application:',
         name: 'screenshot',
         default: 'Include Screenshot'
     },
     {
         type: 'input',
-        message: 'Please include a link to the application',
+        message: 'Please include a link to the application:',
         name: 'link',
         default: 'Enter Link to Application'
     },
     {
         type: 'input',
-        message: 'Please include License information',
+        message: 'Please include License information:',
         name: 'license',
         default: 'Enter License Information',
     },
@@ -65,7 +65,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((responses) => {
-        console.log("Professional ReadMe File Generating");
+        console.log("...Professional README File Generating...");
         writeToFile('./final/README.md', generateMarkdown({...responses}));
     });
 }
