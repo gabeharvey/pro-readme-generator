9038c292f8bb9cb34b01d9c0a4927ffff8488466
Gabe Harvey <harvey7@gmail.com>
2024-01-29 10:10:33 -0600

    packages added, initial question set up in index.js

--- develop/index.js
+++ develop/index.js
@@ -1,7 +1,16 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your application?',
+        name: 'name',
+    }
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
